f0f0e77a88788c390137a6f473e9041baf37867f
Rohit Verma <shimesu.kesuke@gmail.com>
2026-07-15 02:11:18 +0530

    Update auth.api.js

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://hireready-zvh4.onrender.com";
 
 export async function register({username, email, password}){
     try{
